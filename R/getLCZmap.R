7e6dd9e22ba1dbb1fea9f7a62ccecc62de7eb2c5
Max Anjos <maxanjos@campus.ul.pt>
2023-09-21 07:04:00 -0300

    Upgrate functions

--- R/getLCZmap.R
+++ R/getLCZmap.R
@@ -1,16 +1,30 @@
 
-#' Get your LCZ map
+#' Get LCZ Map for Your Area
 #'
-#' @param city name your city
-#' @param roi or use region of interest at ESRI shapefile format
-#' @param isave if you want to save the map.tiff into you machine
+#' This function imports the LCZ (Local Climate Zone) global mapping dataset
+#' performed by Demuzere et al. (2022), available at https://doi.org/10.5194/essd-14-3835-2022.
+#' It automatically clips the LCZ map to your specified area of interest, which can be a city, state, region, or neighborhood.
 #'
-#' @return a raster.tiff with the LCZ classes
+#' @param city The name of your city or the area you want to focus on. It is based on Open Street Map project.
+#' @param roi Optionally, you can provide a Region of Interest (ROI) in ESRI shapefile format to clip the LCZ map.
+#' @param isave Set to TRUE if you want to save the resulting map as a raster.tiff file on your local machine.
+#'
+#' @return A Terra raster.tiff file containing the LCZ classes for your specified area.
+#'
 #' @export
 #'
-#' @examples myLczmap <- getLCZmap(city = "Berlin")
-
-
+#' @examples
+#' #For city
+#' myLczmap <- getLCZmap(city = "Berlin", roi = NULL, isave = TRUE)
+#'
+#' #For neighborhood.
+#' myLczmap <- getLCZmap(city = "Berlin, Mitte", roi = NULL, isave = TRUE)
+#'
+#' # Get LCZ map for a custom region of interest
+#' custom_roi <- st_read("custom_roi.shp")
+#' roi_lcz <- getLCZmap(roi = custom_roi, isave = TRUE)
+#'
+#' @export
 getLCZmap <- function(city=NULL, roi = NULL, isave = TRUE) {
   # Validate inputs
   if (is.null(city) & is.null(roi)) {
@@ -38,26 +52,40 @@ getLCZmap <- function(city=NULL, roi = NULL, isave = TRUE) {
         sf::st_as_sf() %>%
         sf::st_transform(crs="+proj=longlat +datum=WGS84 +no_defs")
     }
-    # Download the LCZ global map from https://zenodo.org/record/6364594/files/lcz_filter_v1.tif?download=1
-    lcz_download <- terra::rast("/vsicurl/https://zenodo.org/record/6364594/files/lcz_filter_v1.tif?download=1")
-    lcz_ras <- terrra::crop(lcz_download, terra::ext(study_area))
+    # Download the LCZ global map
+    lcz_url <- "https://zenodo.org/record/6364594/files/lcz_filter_v1.tif?download=1"
+    lcz_download <- terra::rast(paste0("/vsicurl/", lcz_url))
+    lcz_ras <- terra::crop(lcz_download, terra::ext(study_area))
     lcz_ras <- terra::mask(lcz_ras, terra::vect(study_area))
-    names(lcz_ras) <- paste0("LCZ")
+    names(lcz_ras) <- "lcz"
     return(lcz_ras)
   } else {
     # Download the LCZ global map from https://zenodo.org/record/6364594/files/lcz_filter_v1.tif?download=1
-    lcz_download <- terra::rast("/vsicurl/https://zenodo.org/record/6364594/files/lcz_filter_v1.tif?download=1")
+    lcz_url <- "https://zenodo.org/record/6364594/files/lcz_filter_v1.tif?download=1"
+    lcz_download <- terra::rast(paste0("/vsicurl/", lcz_url))
     roi_crs <- roi %>%
       sf::st_as_sf() %>%
       sf::st_make_valid() %>%
       sf::st_transform(crs = "+proj=longlat +datum=WGS84 +no_defs")
     lcz_ras <- terra::crop(lcz_download, terra::ext(roi_crs))
     lcz_ras <- terra::mask(lcz_ras, terra::vect(roi_crs))
-    names(lcz_ras) <- "LCZ"
+    names(lcz_ras) <- "lcz"
     return(lcz_ras)
 
     if(isave==TRUE){
-      raster::writeRaster(lcz_download, paste0(getwd(), "/lcz_Globalmap.tif"), format="GTiff", overwrite = TRUE)
+
+      # Create a folder name using paste0
+      folder <- paste0("LCZ4r_output/")
+
+      # Check if the folder exists
+      if (!dir.exists(folder)) {
+        # Create the folder if it does not exist
+        dir.create(folder)
+      }
+
+      file <- paste0(getwd(),folder,"lcz_map.tif")
+
+      raster::writeRaster(lcz_download, file, format="GTiff", overwrite = TRUE)
     }
   }
 
--- R/plotLCZmap.R
+++ R/plotLCZmap.R
@@ -1,15 +1,29 @@
 
-#' Plot your LCZ map
+#' Plot LCZ Map
 #'
-#' @param x
+#' This function plots the classes of an LCZ (Local Climate Zone) map represented as a SpatRaster object.
+#' You can obtain the LCZ map using the getLCZmap() function.
 #'
-#' @return
+#' @param x The LCZ map in SpatRaster format.
+#' @param legend Specify whether to include a legend. Set to "code" to show LCZ codes in the legend.
+#'
+#' @return A plot of the LCZ map.
+#'
 #' @export
 #'
 #' @examples
-plotLCZmap <- function(x) {
+#' myLCZmap <- getLCZmap(city = "Berlin")
+#' plotLCZmap(x = myLCZmap, legend = "code")
+#'
+#' @seealso
+#' See the documentation for getLCZmap() to obtain an LCZ map.
+#'
+#' @keywords LCZ, Local Climate Zone, urban climate, spatial analysis
 
-  lcz_map <- x
+
+plotLCZmap <- function(x, legend = "name") {
+
+  lcz_map <- raster::raster(x)
   lczClass <- raster::ratify(lcz_map)
   rat <- raster::levels(lczClass)[[1]]
   ID <- c(seq(1, 10, 1), seq(11, 17)) %>% as_tibble() %>% set_names("ID")
@@ -30,17 +44,25 @@ plotLCZmap <- function(x) {
   color_values <- lcz_df %>% dplyr::select(ID, lcz.col) %>% pull(lcz.col, ID)
 
   # Define LCZ labels
-  lcz.lables <- lcz_df$lcz.name
+  if(legend == "code") {
+    lcz.lables <- lcz_df$lcz
+  } else {
+    lcz.lables <- lcz_df$lcz.name
+  }
 
   # ggplot using the same data
   ggplot() +
     # Add the raster layer
     geom_raster(aes(x = x, y = y, fill = as.factor(class)), data = terra::as.data.frame(lcz_map, xy=TRUE) %>% drop_na(), interpolate = TRUE) +
     # Set the color palette to a qualitative one and add labels, title and legend.hist
-    scale_fill_manual(values = color_values, name = "LCZ class", labels = lcz.lables, guide = guide_legend(reverse = FALSE, title.position = "top")) +
+    scale_fill_manual(values = color_values, name = "Class", labels = lcz.lables, guide = guide_legend(reverse = FALSE, title.position = "top")) +
     # Add layout elements
-    ggtitle("Local Climate Zones") +
-    theme(plot.title = element_text(color = "#3f1651", size = 12, face = "bold"),
+    coord_equal() +
+    labs(title = "Local Climate Zones",
+      caption = "Chart: Max Anjos • Source:©LCZ4 and ggplot2\nData:Demuzere et al.(2022), https://doi.org/10.5194/essd-14-3835-2022",
+         colour = "grey60", size = 8) +
+    theme_void()+
+    theme(plot.title = element_text(color = "#3f1651", size = 14, face = "bold"),
           plot.background = element_rect(fill = "white"),
           legend.position = "bottom",
           legend.box.background = element_rect(colour = "black", size = 0.5, fill = NA),
@@ -53,11 +75,9 @@ plotLCZmap <- function(x) {
           axis.ticks = element_blank(),
           panel.grid.major = element_line(color = "white", size = 0.3),
           panel.grid.minor = element_line(color = "white", size = 0.3),
-          plot.margin = margin(1, 1, 1, 1)) +
-    # add other elements
-    coord_equal() +
-    labs(caption = "Chart: Max Anjos • Source:©ZoomCityCarbonModel\nData:Demuzere et al.(2022), https://doi.org/10.5194/essd-14-3835-2022",
-         colour = "grey60", size = 8) +
-    theme_void()
-
+          plot.margin = margin(1, 1, 1, 1))+
+    ggspatial::annotation_scale(bar_cols = c("grey40", "grey80"), colour = "white")+
+    ggspatial::annotation_north_arrow(location = "br", which_north = "true",
+                                      height = unit(1.0, "cm"),
+                                      width = unit(1.0, "cm"))
 }
